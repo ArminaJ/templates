bdb77dacaa7f1ace347a354891dd7496d4ee620c
ArminaJ <armina_jusufbasic@hotmail.com>
2020-02-07 16:55:07 +0100

    components added

--- source/js/common.js
+++ source/js/common.js
@@ -1,3 +1,8 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-undef */
+/* eslint-disable wrap-iife */
 /*
  * Entry Point for webpack module bundler.
 */
@@ -7,5 +12,9 @@
 *     D E P E N D E N C I E S
 * ==================================
 */
+// eslint-disable-next-line no-unused-vars
+import ScrollFunction from './script/scroll-script';
 
 require('svgxuse');
+
+ScrollFunction();
---
+++ source/js/script/scroll-script.js
@@ -0,0 +1,51 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-loop-func */
+/* eslint-disable arrow-parens */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable wrap-iife */
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-undef */
+export default function ScrollFunction() {
+  const scroll = {
+    init: () => {
+      scroll.dom();
+      scroll.scroll();
+    },
+    dom: () => {
+      scroll.navLinks = document.querySelectorAll('a');
+      scroll.sections = document.querySelectorAll('href');
+    },
+    scroll: () => {
+      const makeNavLinksSmooth = () => {
+        for (let n in scroll.navLinks) {
+          if (scroll.navLinks.hasOwnProperty(n)) {
+            scroll.navLinks[n].addEventListener('click', e => {
+              e.preventDefault();
+              document.querySelector(scroll.navLinks[n].hash).scrollIntoView({
+                behavior: 'smooth',
+              });
+            });
+          }
+        }
+      };
+      makeNavLinksSmooth();
+
+      scroll.navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+          if (this.classList.contains('is-active')) {
+            this.classList.remove('is-active');
+          } else {
+            this.classList.add('active');
+            scroll.navLinks.forEach(l => {
+              if (l !== this) {
+                l.classList.remove('active');
+              }
+            });
+          }
+        });
+      });
+    },
+  };
+  scroll.init();
+}
