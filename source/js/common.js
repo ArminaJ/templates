33196412eb2e3a2b5e891fd7a8741ee7342ab812
ArminaJ <armina_jusufbasic@hotmail.com>
2020-02-19 11:26:11 +0100

    first commit

--- source/js/common.js
+++ source/js/common.js
@@ -1,14 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable radix */
-/* eslint-disable prefer-const */
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable func-names */
-/* eslint-disable no-use-before-define */
-/* eslint-disable wrap-iife */
-/* eslint-disable prefer-template */
-/* eslint-disable prefer-arrow-callback */
-/* eslint-disable no-undef */
 /*
  * Entry Point for webpack module bundler.
 */
@@ -18,8 +7,6 @@
 *     D E P E N D E N C I E S
 * ==================================
 */
-import ScrollFunction from './script/scroll-script';
+
 
 require('svgxuse');
-
-ScrollFunction();
